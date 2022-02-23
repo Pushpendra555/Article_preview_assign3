b694c400d74fe98582757db93a28263ddd5bb457
pushpendra555 <puspendra.t555@gmail.com>
2022-02-23 15:51:12 +0530

    assign-4

---
+++ main.js
@@ -0,0 +1,7 @@
+const shareButton =document.querySelector('#share_image');
+const shareBox =document.querySelector('#share_box');
+
+
+shareButton.addEventListener('click',()=>{
+    shareBox.classList.toggle('hidden');
+})
